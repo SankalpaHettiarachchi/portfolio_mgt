f16221f2c712ff0bb7562237517fe30091c360a2
SankalpaHettiarachchi <itt1819041@tec.rjt.ac.lk>
2023-10-08 12:24:22 +0530

    migratting

---
+++ app/Http/Controllers/project_controller.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\project;
+use Illuminate\Http\Request;
+
+class project_controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = project::all();
+        return view('home')->with('projects', $projects);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'project_name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'project_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', 
+        ]);
+
+        if ($request->hasFile('project_image')) {
+            $imagePath = $request->file('project_image')->store('uploads','public');
+        }
+
+        project::create([
+            'project_name' => $request->input('project_name'),
+            'description' => $request->input('description'),
+            'image' => isset($imagePath) ? $imagePath : null,
+        ]);
+
+        return redirect()->route('home')->with('success', 'Form data submitted successfully');
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
